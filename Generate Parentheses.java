efd717e82a46c15b53b2a34aeb9f27b472723ea4
Sahana Panda <sahana.panda@gmail.com>
2026-07-17 20:08:02 +0530

    Implement generateParenthesis function using backtracking

---
+++ Generate Parentheses.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
+        // Base case: if the current string has reached the maximum allowed length
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        // If we can still add an opening parenthesis
+        if (open < max) {
+            current.append("(");
+            backtrack(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1); // Backtrack
+        }
+
+        // If we can add a closing parenthesis (must be less than open)
+        if (close < open) {
+            current.append(")");
+            backtrack(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1); // Backtrack
+        }
+    }
+}
