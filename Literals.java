4e2b42cc024a34beba2bdd219d00196748f199ea
Sahana Panda <sahana.panda@gmail.com>
2026-08-26 22:07:48 +0530

    Create Literals class with number examples
    Add a class to demonstrate different number literals.

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args) {
+        int decimal = 25;
+        int hexadecimal = 0x1A;
+        int binary = 0b1010;
+        int octal = 012;
+
+        System.out.printf("decimal : %d\n", decimal);
+        System.out.printf("hexadecimal : %d\n", hexadecimal);
+        System.out.printf("binary : %d\n", binary);
+        System.out.printf("octal : %d\n", octal);
+
+    }
+}
