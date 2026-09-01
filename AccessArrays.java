27a70518ee5fbe4cd8e1b10fd5a06f9439914393
Sahana Panda <sahana.panda@gmail.com>
2026-09-01 21:11:42 +0530

    Create AccessArrays.java

---
+++ AccessArrays.java
@@ -0,0 +1,8 @@
+public class AccessArrays {
+    public static void main(String[] args) {
+        
+        int a[] = {10,20,30,40,50};
+
+        System.out.print("Third element is : " + a[2]);
+    }
+}
