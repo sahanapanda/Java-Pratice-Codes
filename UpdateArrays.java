7ccd56c8a4375c1a9fa188a719429437ea4a1414
Sahana Panda <sahana.panda@gmail.com>
2026-09-01 21:14:00 +0530

    Update element in array and print result

---
+++ UpdateArrays.java
@@ -0,0 +1,9 @@
+public class UpdateArrays {
+    public static void main(String[] args) {
+        
+        int a[] = {10,20,30,40,50};
+
+        a[2] = 60;
+        System.out.print("Updated element is : " + a[2]);
+    }
+}
