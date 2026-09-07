5eb5935dcd2c1082334037573e0633e15515da26
Sahana Panda <sahana.panda@gmail.com>
2026-09-07 23:17:58 +0530

    Implement word search using DFS algorithm

---
+++ Word Search.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Start DFS search when the first character matches
+                if (board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        // Base case: found all letters in the word
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Boundary checks and character match check
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        // Mark cell as visited by modifying it temporarily
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // Explore 4 directional neighbors: Up, Down, Left, Right
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                        dfs(board, word, i - 1, j, index + 1) ||
+                        dfs(board, word, i, j + 1, index + 1) ||
+                        dfs(board, word, i, j - 1, index + 1);
+
+        // Backtrack: restore original character
+        board[i][j] = temp;
+
+        return found;
+    }
+}
