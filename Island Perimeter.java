a604e588bf5c68549e5a8f069160e9659123216a
Sahana Panda <sahana.panda@gmail.com>
2026-09-23 23:26:49 +0530

    Implement island perimeter calculation

---
+++ Island Perimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    
+                    // Check if there is land above
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    // Check if there is land to the left
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+}
